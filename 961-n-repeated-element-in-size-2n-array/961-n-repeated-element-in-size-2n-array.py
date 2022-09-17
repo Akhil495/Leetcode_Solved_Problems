31c45883b271e560752f1763fa5692050ac5f358
Akhil Jain <30870421+Akhil495@users.noreply.github.com>
2022-09-17 15:10:28 -0400

    Time: 511 ms (12.93%), Space: 15.3 MB (91.25%) - LeetHub

--- 961-n-repeated-element-in-size-2n-array/961-n-repeated-element-in-size-2n-array.py
+++ 961-n-repeated-element-in-size-2n-array/961-n-repeated-element-in-size-2n-array.py
@@ -1,3 +1,10 @@
 class Solution:
     def repeatedNTimes(self, nums: List[int]) -> int:
-        return collections.Counter(nums).most_common(1)[0][0]
+        # return collections.Counter(nums).most_common(1)[0][0]
+        hash_map = {}
+        for i in nums:
+            if i not in hash_map:
+                hash_map[i] = 1
+            else:
+                return i
+            
